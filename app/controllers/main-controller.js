6f0687f15dafc774568a831e223b8d150412d4fb
LuisDanielCL <daniel.cordero.leonhardes@gmail.com>
2020-05-10 22:13:40 -0600

    service CRUD added

--- app/controllers/main-controller.js
+++ app/controllers/main-controller.js
@@ -112,18 +112,92 @@ exports.getProductById = (req, res) => {
 
 
 exports.createService = (req, res) => {
-
+  if (!req.body.nextServiceDate || !req.body.productId || !req.body.state) {
+    res.status(400).send({
+      message: "nextServiceDate, productId and acquisitionDate cant be empty"
+    });
+    return;
+  }
+  const service = {
+    state: req.body.state,
+    productId: req.body.productId,
+    nextServiceDate: new Date(req.body.nextServiceDate),
+  };
+  Services.create(service).then(data => {
+    res.send(data);
+  }).catch(err => {
+    res.status(500).send({
+      message:
+        err.message || "Something happened while creating a new service"
+    });
+  });
 };
 
 exports.deleteService = (req, res) => {
+  const id = req.params.id;
 
+  Services.destroy({
+    where: { id: id }
+  })
+    .then(num => {
+      res.status(200).send({deleted: num});
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Could not delete Service with id=" + id
+      });
+    });
 };
-
 
 exports.updateService = (req, res) => {
-
+  if (!req.body.id || !req.body.nextServiceDate || !req.body.productId || !req.body.state) {
+    res.status(400).send({
+      message: "id, nextServiceDate, productId and state cant be empty"
+    });
+    return;
+  }
+  const service = {
+    state: req.body.state,
+    productId: req.body.productId,
+    nextServiceDate: new Date(req.body.nextServiceDate),
+  };
+  Services.update(service,{ where: {id: req.body.id } }).then(data => {
+    res.send(data);
+  }).catch(err => {
+    res.status(500).send({
+      message:
+        err.message || "Something happened while updating a new service"
+    });
+  });
 };
 
-exports.readService = (req, res) => {
+exports.readService = (req, res) => { // body/optional {direction, filter, limit, offset }
+  const options = {include: [Products]};
+  if (req.query.filter && req.query.direction) options.order = [[req.query.filter, req.query.direction]];
+  if (req.query.limit) options.limit = parseInt(req.query.limit);
+  if (req.query.offset) options.offset = parseInt(req.query.offset);
 
+  Services.findAll(options)
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving services."
+      });
+    });
+};
+
+exports.countServices = (req, res) => {
+  Services.count({})
+    .then(data => {
+      res.status(200).send({count: data});
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving count of services."
+      });
+    });
 };
--- app/routes/routes.js
+++ app/routes/routes.js
@@ -3,12 +3,20 @@ module.exports = app => {
 
   var router = require("express").Router();
 
+  //Product
   router.post("/product", controller.createProduct);
   router.get("/product", controller.readProduct);
   router.get("/product/:id", controller.getProductById);
   router.delete("/product/:id", controller.deleteProduct);
   router.put("/product", controller.updateProduct);
   router.get("/products/count", controller.countProducts);
+
+  //Service
+  router.post("/service", controller.createService);
+  router.get("/service", controller.readService);
+  router.delete("/service/:id", controller.deleteService);
+  router.put("/service", controller.updateService);
+  router.get("/services/count", controller.countServices);
 
 
   app.use('/api', router);
